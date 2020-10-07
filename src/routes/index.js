7a1e3c525f7f8b322416f998222372fda6201af8
Jacksson Andrey <jaab.andrey@gmail.com>
2020-10-07 16:50:48 -0300

    page login

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import CustomRoute from './CustomRoutes';
+
+import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <CustomRoute exact path="/login" component={Login} />
+      <CustomRoute isPrivate exact path="/dashboard" component={Dashboard} />
+    </Switch>
+  );
+}
