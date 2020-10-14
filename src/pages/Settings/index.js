4ebbc035a0c3c135ed2fd757897c5f283333cdf7
Jacksson Andrey <jaab.andrey@gmail.com>
2020-10-14 18:07:45 -0300

    creation of the method for change password user

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
+
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  TextField,
+  Container,
+  Grid,
+  makeStyles
+} from '@material-ui/core';
 
 import api from '../../services/api';
 import Menus from '../../components/Menus';
 import Copyright from '../../components/Copyright';
+import getCookie from '../../utils/functions';
 
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -52,6 +62,36 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Settings() {
   const classes = useStyles();
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  async function handleChangePassword(e) {
+    e.preventDefault();
+
+    const csrfToken = getCookie('csrftoken');
+
+    const { data, status } = await api.put('/users/change_password/', {
+      old_password: oldPassword,
+      new_password: newPassword
+    }, {
+      headers: {
+        'X-CSRFToken': csrfToken
+      }
+    });
+
+    if (status !== 200) {
+      toast.error('Não foi possível alterar a senha, tente novamente!');
+    }
+
+    if (status === 200) {
+      setNewPassword('');
+      setOldPassword('');
+      toast.success('Senha alterada com sucesso!');
+    }
+
+    console.log(data, status);
+  }
+
   return (
     <div className={classes.root}>
       <ToastContainer />
@@ -60,7 +100,55 @@ export default function Settings() {
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container className={classes.container} maxWidth="lg">
-
+          <form
+            onSubmit={(e) => handleChangePassword(e)}
+          >
+            <Card>
+              <CardHeader
+                subheader="Atualize sua senha"
+                title="Senha"
+              />
+              <Divider />
+              <CardContent>
+                <TextField
+                  fullWidth
+                  label="Senha antiga"
+                  margin="normal"
+                  name="password"
+                  onChange={(e) => setOldPassword(e.target.value)}
+                  type="password"
+                  value={oldPassword}
+                  variant="outlined"
+                  required
+                />
+                <TextField
+                  fullWidth
+                  label="Nova senha"
+                  margin="normal"
+                  name="confirm"
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  type="password"
+                  value={newPassword}
+                  variant="outlined"
+                  required
+                />
+              </CardContent>
+              <Divider />
+              <Box
+                display="flex"
+                justifyContent="flex-end"
+                p={2}
+              >
+                <Button
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                >
+                  Atualizar
+                </Button>
+              </Box>
+            </Card>
+          </form>
 
           <Box pt={4}>
             <Copyright />
