0d6713355b2bfb7f07b380fbcb87fa017684b555
Andrey Araujo <jaab.andrey@gmail.com>
2021-04-30 15:51:44 -0300

    Initial page: modify data of cards

--- src/components/Chart/index.jsx
+++ src/components/Chart/index.jsx
@@ -1,54 +1,70 @@
-import React from 'react';
-import { useTheme } from '@material-ui/core/styles';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Badge
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import Title from '../Title';
+import api from '../../services/api';
 
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
-}
-
-const data = [
-  createData('00:00', 0),
-  createData('03:00', 300),
-  createData('06:00', 600),
-  createData('09:00', 800),
-  createData('12:00', 1500),
-  createData('15:00', 2000),
-  createData('18:00', 2400),
-  createData('21:00', 2400),
-  createData('24:00', undefined),
-];
+const useStyles = makeStyles((theme) => ({
+  seeMore: {
+    marginTop: theme.spacing(3),
+  },
+  link: {
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline'
+    }
+  }
+}));
 
 export default function Chart() {
-  const theme = useTheme();
+  const classes = useStyles();
+
+  const [physicalPersons, setPhysicalPersons] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await api.get('/persons/physical/last/');
+        setPhysicalPersons(data);
+      } catch (err) {
+        const { data } = err.response;
+        console.error(data.detail);
+      }
+    })();
+  }, []);
 
   return (
     <React.Fragment>
-      <Title>Today</Title>
-      <ResponsiveContainer>
-        <LineChart
-          data={data}
-          margin={{
-            top: 16,
-            right: 16,
-            bottom: 0,
-            left: 24,
-          }}
-        >
-          <XAxis dataKey="time" stroke={theme.palette.text.secondary} />
-          <YAxis stroke={theme.palette.text.secondary}>
-            <Label
-              angle={270}
-              position="left"
-              style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
-            >
-              Sales ($)
-            </Label>
-          </YAxis>
-          <Line type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false} />
-        </LineChart>
-      </ResponsiveContainer>
+      <Title>Últimos fornecedores cadastrados</Title>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>CNPJ</TableCell>
+            <TableCell>Razão Social</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {physicalPersons.map((person) => (
+            <TableRow key={person.id_pessoa_cod}>
+              <TableCell>{person.cpfcnpj}</TableCell>
+              <TableCell>{person.nomeorrazaosocial}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className={classes.seeMore}>
+        <Link color="primary" to="/legal/persons" className={classes.link}>
+          Ver mais
+        </Link>
+      </div>
     </React.Fragment>
   );
 }
--- src/components/Deposits/index.jsx
+++ src/components/Deposits/index.jsx
@@ -1,32 +1,51 @@
-import React from 'react';
-import { Link, Typography } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Title from '../Title';
 
-function preventDefault(event) {
-  event.preventDefault();
-}
+import api from '../../services/api';
 
 const useStyles = makeStyles({
   depositContext: {
     flex: 1,
   },
+  link: {
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline'
+    }
+  }
 });
 
 export default function Deposits() {
   const classes = useStyles();
+  const [countUsers, setCountUsers] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await api.get('/users/list');
+        setCountUsers(data.users.length);
+      } catch (err) {
+        const { data } = err.response;
+        console.error(data.detail);
+      }
+    })();
+  }, []);
+
   return (
     <React.Fragment>
-      <Title>Recent Deposits</Title>
-      <Typography component="p" variant="h4">
-        $3,024.00
+      <Title>Usuários ativos na instituição</Title>
+      <Typography component="p" variant="h3">
+        {countUsers}
       </Typography>
       <Typography color="textSecondary" className={classes.depositContext}>
-        on 15 March, 2019
+        usuários
       </Typography>
       <div>
-        <Link color="primary" href="#" onClick={preventDefault}>
-          View balance
+        <Link color="primary" to="/users" className={classes.link}>
+          Ver usuários
         </Link>
       </div>
     </React.Fragment>
